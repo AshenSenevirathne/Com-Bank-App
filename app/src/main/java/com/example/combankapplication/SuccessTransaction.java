8c9c7ebf817909245b11aa5dab76b2f477175708
Dimuthu Abeysighe <DimuthuA@dpinfotech.lk>
2020-10-10 18:34:45 +0530

    Added new file to drawable folder

---
+++ app/src/main/java/com/example/combankapplication/SuccessTransaction.java
@@ -0,0 +1,26 @@
+package com.example.combankapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SuccessTransaction extends AppCompatActivity {
+    Button BtnDone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_transaction);
+
+        BtnDone = findViewById(R.id.btn_submit);
+        BtnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SuccessTransaction.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
