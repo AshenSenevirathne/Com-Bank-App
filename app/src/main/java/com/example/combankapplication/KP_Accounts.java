29c556bc04a3faf3ac6a61616d46652060ff0c07
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-10-10 11:47:31 +0530

    Add KP screen java class

---
+++ app/src/main/java/com/example/combankapplication/KP_Accounts.java
@@ -0,0 +1,14 @@
+package com.example.combankapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KP_Accounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_k_p__accounts);
+    }
+}
