a7c0ee9a808ed64b124483b67035592cf38b8fc6
Dimuthu Abeysighe <DimuthuA@dpinfotech.lk>
2020-10-10 18:38:57 +0530

    Update dashboard

--- app/src/main/java/com/example/combankapplication/KP_Dashboard.java
+++ app/src/main/java/com/example/combankapplication/KP_Dashboard.java
@@ -1,14 +1,35 @@
 package com.example.combankapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class KP_Dashboard extends AppCompatActivity {
-
+    CardView cardFundTrn, cardPaySettl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_k_p__dashboard);
+
+        cardFundTrn = findViewById(R.id.cardView1);
+        cardFundTrn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KP_Dashboard.this, FundTranManagement.class);
+                startActivity(intent);
+            }
+        });
+
+        cardPaySettl = findViewById(R.id.cardView2);
+        cardPaySettl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KP_Dashboard.this, PaymentSettlement.class);
+                startActivity(intent);
+            }
+        });
     }
 }
