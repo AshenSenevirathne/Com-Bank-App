c6e3761b3070f54e18bc6102ec649470de2a4f80
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-10-10 12:09:16 +0530

    added java class

---
+++ app/src/main/java/com/example/combankapplication/KP_Account_details.java
@@ -0,0 +1,14 @@
+package com.example.combankapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KP_Account_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_k_p__account_details);
+    }
+}
