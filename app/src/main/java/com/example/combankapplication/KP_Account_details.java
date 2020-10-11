bb4039edd419fc0457a72d91fbf3f560a3809cf9
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-10-11 14:39:09 +0530

    update back button action

--- app/src/main/java/com/example/combankapplication/KP_Account_details.java
+++ app/src/main/java/com/example/combankapplication/KP_Account_details.java
@@ -1,14 +1,28 @@
 package com.example.combankapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class KP_Account_details extends AppCompatActivity {
 
+    ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_k_p__account_details);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(KP_Account_details.this, KP_Dashboard.class));
+            }
+        });
     }
 }
